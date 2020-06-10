54de4d442c4b84cb45ed5b399fdb819c8ec49bb1
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-10 11:44:33 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,102 @@
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |arr|
+    board[arr[0]] == board[arr[1]] &&
+    board[arr[1]] == board[arr[2]] &&
+    position_taken?(board, arr[0])
+  end
+end
+
+def full?(board)
+  board.none?(" ")
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  win_com = won?(board)
+  win_com ? board[win_com[0]] : nil
+end
+
+def turn_count(board)
+  count = 0 
+  board.each do |el|
+    if el == "X" || el == "O"
+      count += 1 
+    end
+  end
+  return count 
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  turns % 2 == 0 ? "X" : "O"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def play(board)
+  9.times do
+    turn(board)
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
+
+
+
+
+
+
